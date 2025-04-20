a876dfbbaa892687dc1688be62e4bbe64df43e4e
Rugved7 <rugvedagasti7@gmail.com>
2025-04-20 19:24:21 +0530

    User details service implemented

--- Auth_service/app/src/main/java/org/rugved/service/UserDetailsServiceIMPL.java
+++ Auth_service/app/src/main/java/org/rugved/service/UserDetailsServiceIMPL.java
@@ -3,6 +3,7 @@ package org.rugved.service;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.rugved.entities.UserInfo;
+import org.rugved.model.UserInfoDTO;
 import org.rugved.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.UUID;
 
 @Component
 @AllArgsConstructor
@@ -25,9 +30,25 @@ public class UserDetailsServiceIMPL implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserInfo user = userRepository.findByUsername(username);
-        if(user == null){
+        if (user == null) {
             throw new UsernameNotFoundException("Could not find user");
         }
         return new CustomUserDetails(user);
+    }
+
+    public UserInfo checkIfUserAlreadyExists(UserInfoDTO userInfoDTO) {
+        return userRepository.findByUsername(userInfoDTO.getUserName());
+    }
+
+    public Boolean signUpUser(UserInfoDTO userInfoDTO) {
+        userInfoDTO.setPassword(passwordEncoder.encode(userInfoDTO.getPassword()));
+        if (Objects.nonNull(checkIfUserAlreadyExists(userInfoDTO))) {
+            return false;
+        }
+        String userId = UUID.randomUUID().toString();
+        userRepository.save(new UserInfo(userId, userInfoDTO.getUserName(),
+                userInfoDTO.getPassword(), new HashSet<>()
+        ));
+        return true;
     }
 }
